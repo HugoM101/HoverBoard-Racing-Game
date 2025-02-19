126772bc46a04ce5b0319c9872baadfa3404082d
HugoM101 <5macdh24@solent.ac.uk>
2025-02-19 20:35:55 +0000

    smooth WASD movement
    WASD movement added using forces on rigidbody.  board is moving smoothly.

--- Assets/Scripts/Hoverboard.cs
+++ Assets/Scripts/Hoverboard.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Hoverboard : MonoBehaviour
 {
+    public Transform[] hoverPoints;
+    public float hoverHeight;
+    public float hoverForce;
+
+    public float baseSpeed;
+    public float turningSpeed;
+
+    private Rigidbody rb;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Move(float moveInput, float turnInput)
+    {
+        Vector3 moveDirection = transform.forward * moveInput * baseSpeed;
+        rb.AddForce(moveDirection, ForceMode.Acceleration);
+
+        float turnAmount = turnInput * turningSpeed;
+        rb.AddTorque(transform.up * turnAmount, ForceMode.Acceleration);
+
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Hoverboard hoverboard;
+    float moveInput;
+    float turnInput;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
-    void Update()
+
+    //fixed update used as we are applying forces and torques and we need it to be in sync with physics engine
+    void FixedUpdate()
     {
+        moveInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
         
+        hoverboard.Move(moveInput, turnInput);
     }
 }
