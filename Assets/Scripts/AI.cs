1b182986855250a5754494f87b1f33f38bb54145
HugoM101 <5macdh24@solent.ac.uk>
2025-03-05 21:16:13 +0000

    Obstacle Avoidance
    obstacle avoidance for my AI

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -7,7 +7,7 @@ public class AI : MonoBehaviour
 
     public float baseSpeed; 
     public float rotationSharpness; 
-    public Vector3 orientationOffset = Vector3.zero; 
+    public Vector3 orientationOffset; 
 
     public Transform[] hoverPoints; 
     public float hoverHeight; 
@@ -18,6 +18,11 @@ public class AI : MonoBehaviour
 
     private float progress = 0f; 
     private Rigidbody rb;
+
+    public float obstacleAvoidanceDistance;
+    private Vector3 avoidanceOffset;
+    public float avoidanceStrength;
+    public LayerMask obstacleLayer; 
 
     void Start()
     {
@@ -43,6 +48,7 @@ public class AI : MonoBehaviour
     {
         Move();
         Hover(); 
+        ObstacleAvoidance();
     }
 
     void Move()
@@ -60,6 +66,9 @@ public class AI : MonoBehaviour
         Vector3 targetTangent = splineContainer.EvaluateTangent(progress);
         targetTangent = targetTangent.normalized;
 
+        //to apply offset for avoiding obstacles
+        targetPosition += avoidanceOffset;
+
         Vector3 up = splineContainer.EvaluateUpVector(progress);
 
         /*creates a rotation so that the ai's forward direction lines up with the splines tangent 
@@ -72,6 +81,8 @@ public class AI : MonoBehaviour
 
         //smoothly rotate the ai
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * rotationSharpness); //more sharpness = quicker visual turning
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * baseSpeed);
     }
 
     //same physics as player hoverboard script
@@ -94,6 +105,63 @@ public class AI : MonoBehaviour
             {
                 rb.AddForce(Vector3.down * hoverForce, ForceMode.Acceleration);
             }
+        }
+    }
+
+    private void ObstacleAvoidance()
+    {
+        RaycastHit hitFront, hitLeft, hitRight, hitLeft45, hitRight45;
+
+        //the axis of hoverboard asset model was conflictiing with unity
+        Vector3 forward = -transform.right;             //negative X
+        Vector3 left = -transform.forward;              //negative Z
+        Vector3 right = transform.forward;              //positive Z
+        Vector3 left45 = (forward + left).normalized;   //45 degree left
+        Vector3 right45 = (forward + right).normalized; //45 degree right
+
+                                                                                    /*forward avoidance distance 
+                                                                                    shorter to allow the angled ones to 
+                                                                                    have more time to detect obstacles*/
+        bool obstacleAhead = Physics.Raycast(transform.position, forward, out hitFront, obstacleAvoidanceDistance / 2f, obstacleLayer);
+        bool obstacleLeft = Physics.Raycast(transform.position, left, out hitLeft, obstacleAvoidanceDistance, obstacleLayer);
+        bool obstacleRight = Physics.Raycast(transform.position, right, out hitRight, obstacleAvoidanceDistance, obstacleLayer);
+        bool obstacleLeft45 = Physics.Raycast(transform.position, left45, out hitLeft45, obstacleAvoidanceDistance, obstacleLayer);
+        bool obstacleRight45 = Physics.Raycast(transform.position, right45, out hitRight45, obstacleAvoidanceDistance, obstacleLayer);
+
+        Debug.DrawRay(transform.position, forward * obstacleAvoidanceDistance / 2f, Color.cyan);
+        Debug.DrawRay(transform.position, left * obstacleAvoidanceDistance, Color.blue);
+        Debug.DrawRay(transform.position, right * obstacleAvoidanceDistance, Color.red);
+        Debug.DrawRay(transform.position, left45 * obstacleAvoidanceDistance, Color.magenta);
+        Debug.DrawRay(transform.position, right45 * obstacleAvoidanceDistance, Color.yellow);
+
+        if (obstacleAhead)
+        {
+            if (!obstacleRight && !obstacleRight45)
+            {
+                avoidanceOffset = right * avoidanceStrength;
+            }
+
+            else if (!obstacleLeft && !obstacleLeft45)
+            {
+                avoidanceOffset = left * avoidanceStrength;
+            }
+
+            else if (!obstacleRight)
+            {
+                avoidanceOffset = right * avoidanceStrength;
+            }
+
+            else if (!obstacleLeft)
+            {
+                avoidanceOffset = left * avoidanceStrength;
+            }
+            
+        }
+
+        else
+        {
+            //reset offset back to 0 if nothing detected
+            avoidanceOffset = Vector3.Lerp(avoidanceOffset, Vector3.zero, Time.deltaTime * 2f);
         }
     }
 }
