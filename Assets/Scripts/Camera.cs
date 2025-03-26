38e544eac13e7fefd2e3af9df3775fb99de3ab50
HugoM101 <5macdh24@solent.ac.uk>
2025-03-26 02:44:29 +0000

    camera + racetrack mesh fixed

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class CameraEffects : MonoBehaviour
+{
+    public Transform player; 
+    public Vector3 offset; 
+    public float trackingSpeed;
+    public float rotationSpeed;
+    public float normalFOV;
+    public float boostFOV;
+    public float fovChangeSpeed ; 
+
+    private Camera cam;
+    private Hoverboard hoverboard;
+    private Vector3 velocity; 
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+       
+        hoverboard = player.GetComponent<Hoverboard>();
+        cam.fieldOfView = normalFOV; 
+    }
+
+    void FixedUpdate() 
+    {
+        //position relative to player
+        Vector3 targetPosition = player.TransformPoint(offset); 
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, trackingSpeed);
+
+        //susing player rotation + same offset as hoverboard set in hoverboard script. to be modified soon
+        Quaternion targetRotation = player.rotation * Quaternion.Euler(0, 90, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed);
+
+        //adjusting fov based on if boosting
+        float targetFOV;
+        if (hoverboard.IsBoosting)
+        {
+            targetFOV = boostFOV; 
+        }
+        else
+        {
+            targetFOV = normalFOV; 
+        }
+
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, targetFOV, Time.fixedDeltaTime * fovChangeSpeed);
+    }
+}
